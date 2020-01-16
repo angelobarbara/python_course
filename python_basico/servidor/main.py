a0b3ce88dbd1008b07dca5bc6f78509d01a0cc97
angelobarbara <angelo.iesgrancapitan@gmail.com>
2020-01-16 10:58:01 +0100

    agenda de contactos

--- python_basico/servidor/main.py
+++ python_basico/servidor/main.py
@@ -1,11 +1,40 @@
-from flask import Flask
+from flask import Flask, render_template, request, flash, redirect
+from contact_model import Contact
 
 app = Flask(__name__)
+app.secret_key = 'some_secret'
+app.debug = True
 
+@app.route(r'/', methods=['GET'])
+def contact_book():
+    contacts = Contact.query().fetch()
+    return render_template('contact_book.html', contacts=contacts)
 
-@app.route('/')
-def hello_world():
-    return 'Hola Mundo.'
+@app.route(r'/add', methods=['GET','POST'])
+def add_contact():
+
+    if request.form:
+        contact = contact(name=request.form.get('name'),phone=request.form.get('phone'),email=request.form.get('email'))
+        contact.put()
+        flash('¡Se añadió el contacto!')
+
+    return render_template('add_contact.html')
+
+@app.route(r'/contacts/<uid>', methods=['GET'])
+def contact_details(uid):
+    contact = Contact.query().get_by_id(int(uid))
+
+    if not contact:
+        #Code 301 es una redirección permanente
+        return render_template('/', code=301)
+
+    return render_template('contact.html', contact=contact)
+
+@app.route(r'/delete', methods=['POST'])
+def delete_contact(uid):
+    contact = Contact.get_by_id(int(request.form.get('uid')))
+    contact.key.delete()
+    return redirect('/contacts/{}'.format(contact.key.id()))
 
 if __name__ == '__main__':
     app.run()
